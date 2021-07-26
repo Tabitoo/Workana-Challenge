dce525ea4b476a19f7b061606fe95bfd118b50ff
Tabitoo <lautarovaldez4@gmail.com>
2021-07-26 10:28:48 -0300

    primer commit

---
+++ backend_node/index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 8082;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+
+app.get('/issue/:issue', function(req, res) { 
+    return res.json({"data" : "hola buenos esto es una prueba"})//res.json({issue: parseInt(req.params.issue,10)})
+})
+
+
+app.listen(port, () => {
+    console.log(`Backend node listen in port ${port}`);
+});
